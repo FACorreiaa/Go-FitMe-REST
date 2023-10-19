fc7ddf69497fe89f1f9094274746cd68ec654d02
FACorreiaa <fernandocorreia316@gmail.com>
2023-10-19 21:25:34 +0100

    change some routes

--- api/internal_api/activity/routes.go
+++ api/internal_api/activity/routes.go
@@ -10,18 +10,18 @@ func RoutesActivity(s *StructActivity) *chi.Mux {
 	router := chi.NewRouter()
 
 	router.Get("/", h.GetActivities)
-	router.Get("/id/{id}", h.GetActivitiesById)
-	router.Get("/name/{name}", h.GetActivitiesByName)
-	router.Get("/user/exercises/user/{user_id}", h.GetUserExerciseSession)
-	router.Get("/user/session/total/user/{user_id}", h.GetUserExerciseTotalData)
+	router.Get("/{id}", h.GetActivitiesById)
+	router.Get("/{name}", h.GetActivitiesByName)
+	router.Get("/user/exercises/{user_id}", h.GetUserExerciseSession)
+	router.Get("/user/session/total/{user_id}", h.GetUserExerciseTotalData)
 
-	router.Get("/user/session/stats/user/{user_id}", h.GetUserExerciseSessionStats)
-	router.Get("/user/session/stats/total/user/{user_id}", h.GetExerciseSessionStats)
+	router.Get("/user/session/stats/{user_id}", h.GetUserExerciseSessionStats)
+	router.Get("/user/session/stats/total/{user_id}", h.GetExerciseSessionStats)
 
-	router.Post("/start/session/id/{id}", h.StartActivityTracker)
-	router.Post("/pause/session/id/{id}", h.PauseActivityTracker)
-	router.Post("/resume/session/id/{id}", h.ResumeActivityTracker)
-	router.Post("/stop/session/id/{id}", h.StopActivityTracker)
+	router.Post("/start/session/{id}", h.StartActivityTracker)
+	router.Post("/pause/session/{id}", h.PauseActivityTracker)
+	router.Post("/resume/session/{id}", h.ResumeActivityTracker)
+	router.Post("/stop/session/{id}", h.StopActivityTracker)
 
 	return router
 }
